3ae0135a047a795e11dcf7ef023e8d3e2bba568e
paul-py <paulnwatu@29.com>
2025-05-21 13:03:07 -0700

    complete project

---
+++ index.js
@@ -0,0 +1,26 @@
+window.onload = populateQuote()
+
+let quoteNumber = document.getElementById('quote-number')
+let quote = document.getElementById('quote')
+let quoteBtn = document.getElementById('generate-quote-btn')
+
+quoteBtn.addEventListener("click", populateQuote)
+
+
+
+function populateQuote() {
+    fetch('https://api.adviceslip.com/advice')
+
+        .then(response => {
+            return response.json()
+        })
+
+        .then(data => {
+            let name = data.slip
+
+            quoteNumber.innerHTML = " ADVICE " + name.id
+            quote.innerHTML = name.advice
+        })
+}
+
+
